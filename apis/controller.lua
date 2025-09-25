68c7ffa167404f6f58ddf9899421e61a75e2fd00
Kuberow <kuberow.kuberow@gmail.com>
2025-09-25 19:09:30 +0300

    Create controller.lua

---
+++ apis/controller.lua
@@ -0,0 +1,19 @@
+modem = peripheral.find("modem")
+id = tostring(os.getComputerID)
+-- Check for modem
+if modem then
+	modemSide = peripheral.getName(modem)
+	if not rednet.isOpen() then rednet.open(modemSide) end
+else
+	return "No modem available"
+end
+
+api = {}
+
+rednet.host("KCP", id)
+
+function api.getKey()
+return rednet.receive(0.01)
+end
+
+return api
